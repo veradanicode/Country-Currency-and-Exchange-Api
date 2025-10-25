8d52fb32fdcf64c538756e5ac5a35c3ef1c6593d
Vera D <veradanicode@gmail.com>
2025-10-25 09:12:48 +0100

    add database

---
+++ database/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const connectToDatabase=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database Connected Sucessfully!");
+        
+    } catch (error) {
+        console.log("Database Connection failed: ",error);
+        
+    }
+}
+
+module.exports=connectToDatabase;
